f039ac8ed6af333bb0c386e5acfab7eb30434134
5yoda5 <5timur5@mail.ru>
2026-03-11 00:40:56 +0500

    ramips: add support for Cudy WR1300S
    - SoC: MediaTek MT7621AT
    - WiFi: MT7603 (2.4GHz), MT7615E (5GHz)
    - Flash: 16 MB SPI NOR
    - USB: 1x USB 3.0 (GPIO 7 controlled VBUS)
    
    Changes:
    - Added mt7621_cudy_wr1300s.dts with native USB VBUS and MAC via nvmem.
    - Fixed USB VBUS polarity (ACTIVE_HIGH) to ensure power on boot.
    - Configured LEDs and DSA switch ports mapping.
    - Included block-mount and kmod-fs-vfat in default device packages.

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -3865,6 +3865,7 @@ define Device/cudy_wr1300s
   DEVICE_DTS := mt7621_cudy_wr1300s
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615e kmod-mt7615-firmware \
 	mt7663-firmware-ap wpad-basic-mbedtls opkg \
-	kmod-usb3 kmod-usb-storage usbutils kmod-fs-vfat
+	kmod-usb3 kmod-usb-storage usbutils kmod-fs-vfat \
+	block-mount kmod-fs-ext4 kmod-fs-ntfs3
 endef
 TARGET_DEVICES += cudy_wr1300s
